d64746ec8f4bce43a3b6cf77a266ebf50d630dbe
itsuki-jp <itsuki19601@gmail.com>
2024-12-04 22:50:10 +0900

    add: コメントの追加

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// =====================
+// 必要な変数を定義
+// =====================
+
 // 定数の定義
 const CANVAS_HEIGHT = 1000;
 const CANVAS_WIDTH = 1000;
@@ -5,6 +9,7 @@ const offset = 200;
 const lineSpace = 110;
 const lineWidth = 10;
 const nodeSize = { x: 100, y: 40 };
+// 落ちてくるノードの定義
 const colors = ["red", "blue", "yellow", "purple", "orange"];
 const NODES = colors.map((color, i) => ({
     line: i,
@@ -13,22 +18,22 @@ const NODES = colors.map((color, i) => ({
     size: nodeSize,
     checked: false,
 }));
+// ラインの定義
 const LINES = Array.from({ length: 6 }, (_, i) => ({
     line: i,
     size: { x: lineWidth, y: CANVAS_HEIGHT },
     pos: { x: offset + lineSpace * i, y: 0 }
 }));
-
+// 判定エリアの定義
 const checkArea = Array.from({ length: 5 }, (_, i) => ({
     line: i,
     size: nodeSize,
     pos: { x: offset + lineWidth + lineSpace * i, y: CANVAS_HEIGHT - 100 }
 }));
-
+// 現在落ちてきてるノードの定義
 const CURRENT_NODES = [];
+// スコアの定義(判定エリアにノードがあるときに、特定のキーを押したら+1)
 let score = 0;
-
-
 
 // canvasをJavaScriptで使えるようにする
 const canvas = document.getElementById("canvas");
@@ -38,31 +43,41 @@ const ctx = canvas.getContext("2d");
 canvas.height = 1000;
 canvas.width = 1000;
 
-ctx.fillStyle = "green"; // 色を緑に
-ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+// =====================
+// メインの処理
+// =====================
 
-
+// 四角を描画する関数
+// 引数：書きたい四角、色、透明度（書かなくてもOK）
 const drawRect = (rect, color, alpha = 1) => {
     ctx.globalAlpha = alpha;
     ctx.fillStyle = color;
     ctx.fillRect(rect.pos.x, rect.pos.y, rect.size.x, rect.size.y);
     ctx.globalAlpha = 1;
 }
+
+// ノードを動かす関数
+// 引数：動かしたいノード
 const moveNode = (node) => {
     node.pos.y += 1;
 }
 
-const isCollided = (node, line) => {
-    if (node.line !== line.line) return false;
+// ノードと判定エリアが衝突しているかどうかを判定する関数
+// 引数：ノード、判定エリア
+const isCollided = (node, area) => {
+    // ノードが判定エリアのラインにあるか確認
+    if (node.line !== area.line) return false;
+    // 既に判定してたら無視する
     if (node.checked) return false;
-    if (line.pos.y <= node.pos.y && node.pos.y <= line.pos.y + line.size.y) {
+    // ノードが判定エリアにある場合は、ノードを確認済みにする
+    if (area.pos.y <= node.pos.y && node.pos.y <= area.pos.y + area.size.y) {
         node.checked = true;
-        console.log("aaaaaaaaaaaa")
         return true;
     }
     return false;
 }
 
+// キーが押されたときの処理
 document.addEventListener("keydown", (e) => {
     if (e.key === "a") {
         CURRENT_NODES.forEach((node) => {
@@ -91,10 +106,15 @@ document.addEventListener("keydown", (e) => {
     }
 }
 )
-
+// 今の秒数的な
 let now = 0;
 
+// =====================
+// 10msごとに実行する処理
+// =====================
+
 setInterval(() => {
+    // 盤面を初期化する
     ctx.fillStyle = "green";
     ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     LINES.forEach((line) => {
@@ -103,6 +123,7 @@ setInterval(() => {
     checkArea.forEach((area) => {
         drawRect(area, "gray", 0.7);
     });
+    // ノードを追加する
     for (let i = 0; i < NODES.length; i++) {
         if (NODES[i].time <= now / 10) {
             const newNode = NODES.shift();
@@ -112,7 +133,13 @@ setInterval(() => {
             break;
         }
     }
+    // ノードを動かして、描写する
     CURRENT_NODES.forEach((node) => {
+        // ノードが盤外に移動したら削除する
+        if (node.pos.y + node.size.y > CANVAS_HEIGHT) {
+            CURRENT_NODES.shift();
+            return;
+        }
         moveNode(node);
         drawRect(node, node.color);
     });
